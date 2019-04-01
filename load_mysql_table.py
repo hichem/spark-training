1ebd54506a4d7094ceacbb9596842045a7086890
Hichem BOUSSETTA <Hichem.BOUSSETTA@ingenico.com>
2019-04-01 15:35:04 +0200

    minor update to load_mysql_table.py

--- load_mysql_table.py
+++ load_mysql_table.py
@@ -5,5 +5,5 @@ from pyspark.sql import SQLContext
 sc=SparkContext("local[*]",appName="app")
 sqlContext = SQLContext(sc)
 
-df_mysql = sqlContext.read.format("jdbc").option("url", "jdbc:mysql://localhost/retail_db").option("driver", "com.mysql.jdbc.Driver").option("dbtable", "customers").option("user", "root").option("cloudera", "password").load()
+df_mysql = sqlContext.read.format("jdbc").option("url", "jdbc:mysql://localhost/retail_db").option("driver", "com.mysql.jdbc.Driver").option("dbtable", "customers").option("user", "root").option("password", "cloudera").load()
 df_mysql.show()
